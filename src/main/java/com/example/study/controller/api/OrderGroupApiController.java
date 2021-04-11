d3546923f16fc2a3059d7ad83c0be1c3809eaebd
cyh1219 <cyh1219@naver.com>
2021-04-11 22:47:44 +0900

    9-05.service 추상화적용 2

--- src/main/java/com/example/study/controller/api/OrderGroupApiController.java
+++ src/main/java/com/example/study/controller/api/OrderGroupApiController.java
@@ -19,6 +19,7 @@ alter table `study`.`settlement` add sum_price varchar(100) not null default '0'
 package com.example.study.controller.api;
 
 import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.OrderGroup;
 import com.example.study.model.network.request.OrderGroupApiRequest;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.response.OrderGroupApiResponse;
@@ -29,39 +30,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orderGroup")
-@RequiredArgsConstructor
-public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
-
-    @Autowired
-    private final OrderGroupApiLogicService orderGroupApiLogicService;
+public class OrderGroupApiController extends CrudController<OrderGroupApiRequest, OrderGroupApiResponse, OrderGroup> {
 
 
-    @Override
-    @PostMapping("")
-    public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}")
-    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
-        return orderGroupApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return orderGroupApiLogicService.delete(id);
-    }
-
-//    @Override
-//    public Header<OrderGroupApiResponse> sum(@PathVariable Long id){
-//        return orderGroupApiLogicService.sum(id);
-//    }
 }
